a7c4186d3ec2de6b54cbccab2e7f21a7f2bf2f69
Vidar Magnusson <git@vidarmagnusson.com>
2026-02-05 19:44:16 +0100

    More work

--- packages/ui/src/main_view.rs
+++ packages/ui/src/main_view.rs
@@ -1,6 +1,7 @@
 use dioxus::prelude::*;
 
 const MAIN_SCSS: Asset = asset!("/assets/styling/main.scss");
+const MUSICAL_NOTE_SVG: Asset = asset!("/assets/icons/musical_note.svg");
 
 #[component]
 pub fn Main() -> Element {
@@ -12,8 +13,27 @@ pub fn Main() -> Element {
                 h1 { "Albumklubben" }
             }
 
-            div { class: "first-row",
-                div { class: "card", "asd" }
+            div { class: "first-row row",
+                div { class: "double-column",
+                    div { class: "card",
+                        div { class: "gap-2",
+                            img {
+                                src: MUSICAL_NOTE_SVG,
+                                alt: "musical-note",
+                                class: "note-icon",
+                            }
+                        }
+                        div { class: "gap-2" }
+                    }
+                }
+
+                div {
+                    div { class: "card", "bsd" }
+                }
+            }
+
+            div { class: "row",
+                div { class: "card", "PEPE" }
             }
         }
     }
