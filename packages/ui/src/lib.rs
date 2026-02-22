85d2491876d77501a620a99d7978de18893de495
Vidar Magnusson <git@vidarmagnusson.com>
2026-02-22 15:40:48 +0100

    Fix(?) for mobile

--- packages/ui/src/lib.rs
+++ packages/ui/src/lib.rs
@@ -8,6 +8,7 @@ const GLOBAL_SCSS: Asset = asset!("/assets/styling/globals.scss");
 #[component]
 pub fn Setup() -> Element {
     rsx! {
+        document::Meta { name: "viewport", content: "width=device-width, initial-scale=1" }
         document::Link { rel: "stylesheet", href: GLOBAL_SCSS }
     }
 }
